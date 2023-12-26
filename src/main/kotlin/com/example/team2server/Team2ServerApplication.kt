11e98befb9714022691b3f6a3b9f674fe818b742
asp345 <dlacksdud2@snu.ac.kr>
2023-12-26 11:13:00 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/team2server/Team2ServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.team2server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Team2ServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<Team2ServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/team2server/Team2ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.team2server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team2ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
