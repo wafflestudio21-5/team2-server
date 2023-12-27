3e0ac8eb3c58ed81b6904fc5aceda15a54d73c2d
Chanyeong Lim <dlacksdud2@gmail.com>
2023-12-27 15:18:24 +0900

    초기 프로젝트 설정 (#2)
    * Initial commit
    
    * add more to gitignore
    
    * Create README.md
    
    * 패키지명 및 build.gradle 정리
    
    ---------
    
    Co-authored-by: sstto <youngin.sim@navercorp.com>

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.2.1"
+    id("io.spring.dependency-management") version "1.1.4"
+    kotlin("jvm") version "1.9.21"
+    kotlin("plugin.spring") version "1.9.21"
+    kotlin("plugin.jpa") version "1.9.21"
+}
+
+group = "com.wafflestudio"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+	// language
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	// spring
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	// json
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	// db
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("com.mysql:mysql-connector-j")
+	// test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += "-Xjsr305=strict"
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/kotlin/com/wafflestudio/team2server/Team2ServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.wafflestudio.team2server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Team2ServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
