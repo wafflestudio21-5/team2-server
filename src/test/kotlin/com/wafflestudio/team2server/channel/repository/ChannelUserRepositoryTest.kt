fa3234d8bb120de32d7ac393fe30197875ba8976
sstto <youngin.sim@navercorp.com>
2024-01-03 22:27:27 +0900

    [NO-TSK] @SpringBootTest 제거 - 빌드시 test 포함안되도록 조치 필요

--- src/test/kotlin/com/wafflestudio/team2server/channel/repository/ChannelUserRepositoryTest.kt
+++ src/test/kotlin/com/wafflestudio/team2server/channel/repository/ChannelUserRepositoryTest.kt
@@ -1,18 +1,21 @@
 package com.wafflestudio.team2server.channel.repository
 
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class ChannelUserRepositoryTest @Autowired constructor(
-	private val channelUserRepository: ChannelUserRepository
-){
-
-	@Test
-	fun `쿼리문 확인`() {
-		val res = channelUserRepository.findAllByUserIdWithJoinFetch(1)
-		println(res.size)
-
-	}
-}
+//@Disabled
+//@SpringBootTest
+//class ChannelUserRepositoryTest @Autowired constructor(
+//	private val channelUserRepository: ChannelUserRepository
+//){
+//
+//	@Disabled
+//	@Test
+//	fun `쿼리문 확인`() {
+//		val res = channelUserRepository.findAllByUserIdWithJoinFetch(1)
+//		println(res.size)
+//
+//	}
+//}
