ddce221c673c7b99a8e9173efdbdfd8cc6a88106
sstto <youngin.sim@navercorp.com>
2024-01-05 00:32:36 +0900

    [NO-TSK] 테스트 빌드 제외 및 테스트 코드 원복

--- src/test/kotlin/com/wafflestudio/team2server/channel/repository/ChannelUserRepositoryTest.kt
+++ src/test/kotlin/com/wafflestudio/team2server/channel/repository/ChannelUserRepositoryTest.kt
@@ -1,21 +1,18 @@
 package com.wafflestudio.team2server.channel.repository
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-//@Disabled
-//@SpringBootTest
-//class ChannelUserRepositoryTest @Autowired constructor(
-//	private val channelUserRepository: ChannelUserRepository
-//){
-//
-//	@Disabled
-//	@Test
-//	fun `쿼리문 확인`() {
-//		val res = channelUserRepository.findAllByUserIdWithJoinFetch(1)
-//		println(res.size)
-//
-//	}
-//}
+@SpringBootTest
+class ChannelUserRepositoryTest @Autowired constructor(
+	private val channelUserRepository: ChannelUserRepository
+){
+
+	@Test
+	fun `쿼리문 확인`() {
+		val res = channelUserRepository.findAllByUserIdWithJoinFetch(1)
+		println(res.size)
+	}
+
+}
