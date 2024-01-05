1f69392f5f896fc693f306293e67f7302a1dd387
asp345 <dlacksdud2@snu.ac.kr>
2024-01-06 01:48:52 +0900

    [TSK-86] Fix: AreaService Interface 수정

--- src/main/kotlin/com/wafflestudio/team2server/area/service/AreaService.kt
+++ src/main/kotlin/com/wafflestudio/team2server/area/service/AreaService.kt
@@ -1,9 +1,11 @@
 package com.wafflestudio.team2server.area.service
 
+import com.wafflestudio.team2server.area.model.AreaEntity
 import org.springframework.stereotype.Service
 
 
 @Service
 interface AreaService {
 	fun getAdjAreas(id: Long, distance: Int): Set<Long>
+	fun getAreaById(id: Long): AreaEntity
 }
