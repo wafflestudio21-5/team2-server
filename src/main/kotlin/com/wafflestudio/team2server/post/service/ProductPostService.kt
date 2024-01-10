1bf67d3d9a3c2816e53a214dc123cba8f71fe46d
asp345 <dlacksdud2@snu.ac.kr>
2024-01-11 00:21:22 +0900

    [NO-TSK] Fix: refAreaId 0번 가져오게 수정

--- src/main/kotlin/com/wafflestudio/team2server/post/service/ProductPostService.kt
+++ src/main/kotlin/com/wafflestudio/team2server/post/service/ProductPostService.kt
@@ -2,19 +2,18 @@ package com.wafflestudio.team2server.post.service
 
 import com.wafflestudio.team2server.post.controller.ProductPostController
 import com.wafflestudio.team2server.post.model.ProductPost
-import com.wafflestudio.team2server.post.repository.ProductPostEntity
-import com.wafflestudio.team2server.user.repository.UserEntity
+import com.wafflestudio.team2server.user.model.User
 
 interface ProductPostService {
 	fun exists(id: Long): Boolean
-	fun searchPostByKeyword(cur: Long, title: String, refAreaId: List<Int>, distance: Int): ProductPostController.ListResponse
+	fun searchPostByKeyword(cur: Long, keyword: String, refAreaId: List<Int>, distance: Int): ProductPostController.ListResponse
 	fun create(postCreateRequest: ProductPostController.PostCreateRequest, userId: Long)
 	fun update(postUpdateRequest: ProductPostController.PostUpdateRequest, userId: Long, id: Long, refresh: Boolean)
 	fun findPostById(id: Long): ProductPost
 	fun deleteById(id: Long)
 	fun likePost(userId: Long, id: Long)
 	fun unlikePost(userId: Long, id: Long)
-	fun getLikedPosts(userId: Long): List<ProductPostEntity>
-	fun getLikedUsers(postId: Long): List<UserEntity>
+	fun getLikedPosts(userId: Long): List<ProductPost>
+	fun getLikedUsers(postId: Long): List<User>
 	fun getPostListRandom(cur: Long, seed: Int, areaId: Int, distance: Int): ProductPostController.ListResponse
 }
