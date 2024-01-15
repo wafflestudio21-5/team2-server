0d35fb04b406fe6e0670d5c396c11de99e4b3a42
sstto <sspjyins@snu.ac.kr>
2024-01-15 22:43:33 +0900

    Update ProductPostRepository.kt

--- src/main/kotlin/com/wafflestudio/team2server/post/repository/ProductPostRepository.kt
+++ src/main/kotlin/com/wafflestudio/team2server/post/repository/ProductPostRepository.kt
@@ -15,7 +15,7 @@ interface ProductPostRepository : JpaRepository<ProductPostEntity, Long> {
 
 	@Query(
 		"""select /*+no_merge(t)*/ * from product_post
-			join (select product_post.id, floor(1000*rand(:seed))+id as end fro product_post
+			join (select product_post.id, floor(1000*rand(:seed))+id as end from product_post
 			where hidden_yn=0 and product_post.selling_area_id in (:adjAreaIdList) limit 5000) as t
 			on product_post.id=t.id where end<:cur order by end desc limit 16""",
 		nativeQuery = true
