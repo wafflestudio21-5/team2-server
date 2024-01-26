06b7915dbd61f05e72a4f469bd913b862d15ce2b
onemeee <dlgkssk38@snu.ac.kr>
2024-01-27 01:25:20 +0900

    [TSK-133] Feat: 커뮤니티 업데이트 사진 추가

--- src/main/kotlin/com/wafflestudio/team2server/community/model/CommunityUpdateRequest.kt
+++ src/main/kotlin/com/wafflestudio/team2server/community/model/CommunityUpdateRequest.kt
@@ -1,6 +1,8 @@
 package com.wafflestudio.team2server.community.model
 
 data class CommunityUpdateRequest(
-	val title: String?,
-	val description: String?,
+	val title: String? = null,
+	val description: String? = null,
+	val repImg: String? = null,
+	val images: List<String>? = null
 )
