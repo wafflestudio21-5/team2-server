267fab8d902c0365568ae8ebd22f3ba790b7ad8b
sstto <sspjyins@snu.ac.kr>
2024-01-30 23:20:48 +0900

    [NO-TSK] 지역 조회

--- src/main/kotlin/com/wafflestudio/team2server/area/controller/AreaController.kt
+++ src/main/kotlin/com/wafflestudio/team2server/area/controller/AreaController.kt
@@ -1,16 +1,21 @@
 package com.wafflestudio.team2server.area.controller
 
+import com.wafflestudio.team2server.area.repository.AreaEntity
+import com.wafflestudio.team2server.area.service.Area
 import com.wafflestudio.team2server.area.service.AreaService
 import com.wafflestudio.team2server.area.service.SearchAreaResponse
 import com.wafflestudio.team2server.common.error.BadRequestException
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class AreaController(private val areaService: AreaService) {
-	@GetMapping("/area")
-	fun getAreaInfo(code: String) {
+	@GetMapping("/area/{id}")
+	fun getAreaInfo(@PathVariable id: Int): Area {
+		val area = areaService.getAreaById(id)
+		return Area(area.id, area.code, area.name, area.fullName)
 	}
 
 	@GetMapping("/area/search")
