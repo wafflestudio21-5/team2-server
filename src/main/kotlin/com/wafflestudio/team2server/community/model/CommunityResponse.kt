77aa4b7f1201f5cc08065ef408a5ad127af5f01e
onemeee <dlgkssk38@snu.ac.kr>
2024-01-31 23:24:04 +0900

    [NO-TSK] Fix: 커뮤니티 상세 조회 닉네임, 지역정보, 프로필이미지 추가

--- src/main/kotlin/com/wafflestudio/team2server/community/model/CommunityResponse.kt
+++ src/main/kotlin/com/wafflestudio/team2server/community/model/CommunityResponse.kt
@@ -2,5 +2,8 @@ package com.wafflestudio.team2server.community.model
 
 data class CommunityResponse (
 	val community: Community,
-	val isLiked: Boolean
+	val isLiked: Boolean,
+	val nickname: String,
+	val profileImg: String?,
+	val areaInfo: String
 )
